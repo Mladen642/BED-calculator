1ad3d1b3abe1c70a5256c8b7c2bafc7ff1366093
Mladen <mladenbabic642@protonmail.com>
2022-11-05 18:00:06 +0100

    final

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-def BED(n,d,alfabeta):
+def BED(n, d, alfabeta):
 
-    #racun
-    BED = round(float(n*d*(1 + (d/(alfabeta)))),2)
-    
-    print(f'Ukupan BED iznosi {BED} Gy!')
-    
-if __name__ == '__main__':
-    #broj frakcija  
-    n = int(input('Broj frakcija: '))
+    # racun
+    BED = round(float(n * d * (1 + (d / (alfabeta)))), 2)
 
-    #doza po frakciji
-    d = float(input('Doza po frakciji: '))
-    
-    alfabeta = int(input('alfa/beta: '))
-    
-    BED(n,d,alfabeta)
+    print(f"Ukupan BED iznosi {BED} Gy!")
+
+
+if __name__ == "__main__":
+    # broj frakcija
+    n = int(input("Broj frakcija: "))
+
+    # doza po frakciji
+    d = float(input("Doza po frakciji: "))
+
+    alfabeta = int(input("alfa/beta: "))
+
+    BED(n, d, alfabeta)
